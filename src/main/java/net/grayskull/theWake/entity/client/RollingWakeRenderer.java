b36072150e4ddb6901bc2b14fb5b29ba9685dfbe
Liamgrayskull <liamgrayskulll@gmail.com>
2026-02-01 18:55:56 -0800

    Initial Entity Push! It's broken but it does in fact spawn!

--- src/main/java/net/grayskull/theWake/entity/client/RollingWakeRenderer.java
+++ src/main/java/net/grayskull/theWake/entity/client/RollingWakeRenderer.java
@@ -1,22 +1,16 @@
+
 package net.grayskull.theWake.entity.client;
 
-import net.grayskull.theWake.entity.custom.RollingWakeEntity;
-import net.minecraft.client.model.EntityModel;
-import net.minecraft.client.model.GhastModel;
-import net.minecraft.client.model.geom.ModelLayers;
-import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.client.renderer.entity.MobRenderer;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.Entity;
-/*
-public class RollingWakeRenderer extends MobRenderer {
-    public RollingWakeRenderer(EntityRendererProvider.Context pContext, EntityModel pModel, float pShadowRadius) {
-        super(pContext, new GhastModel(pContext.bakeLayer(ModelLayers.GHAST)), 2f); // replace with modded at later date
-    }
 
-    @Override
-    public ResourceLocation getTextureLocation(Entity pEntity) {
-        return null;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.entity.LightningBoltRenderer;
+
+
+public class RollingWakeRenderer extends LightningBoltRenderer {
+
+
+    public RollingWakeRenderer(EntityRendererProvider.Context pContext) {
+        super(pContext);
     }
 
 
@@ -25,4 +19,3 @@ public class RollingWakeRenderer extends MobRenderer {
 
 
 }
-*/
